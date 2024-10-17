60468cd65c4fee8c294d19e7be7de263ce8e2cd1
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-10-17 09:32:16 +0300

    Deployment commit

---
+++ b9835dc810286343e77c.js
@@ -0,0 +1,9 @@
+import "./style.css"
+import {manageDialog} from "./modules/manageDialog"
+import { displayTasks } from "./modules/display"
+import { formatDate } from "./modules/formatDate"
+
+manageDialog()
+displayTasks()
+// console.log(formatDate());
+
